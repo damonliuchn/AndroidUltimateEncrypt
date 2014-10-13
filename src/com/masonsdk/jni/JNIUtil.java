d1210c5d3f9167fa51a2adcbe2f562bbd84bdf44
MasonLiuChn <MasonLiuChn@gmail.com>
2014-10-13 17:13:05 +0800

    cache

--- src/com/masonsdk/jni/JNIUtil.java
+++ src/com/masonsdk/jni/JNIUtil.java
@@ -6,7 +6,14 @@ public class JNIUtil {
 	static {
 		System.loadLibrary("masonsdkjni");
 	}
-
+	public static String pwd;
+	public static String getCachePwd(Context con){
+		if (pwd==null) {
+			pwd=getPWD(con);
+		}
+		return pwd;
+	}
+	
 	public static native String getPWD(Context con);
 
 	public native String stringFromJNI2(String str);
